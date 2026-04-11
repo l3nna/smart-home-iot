09d4b6c5d5fed9f371157381f90217d3acffa8e7
l3nna <hkojic50@gmail.com>
2026-04-11 17:35:22 +0200

    installed some libraries and fixed  code

--- backend/services/pythonService.js
+++ backend/services/pythonService.js
@@ -1,16 +1,16 @@
 const { exec } = require("child_process");
 
 function runPython(command) {
-    return new Promise((resolve, reject) => {
-        exec('python ../hardware/main.py ${command}', (error, stdout, stderr) => {
-             if (err) {
+  return new Promise((resolve, reject) => {
+    exec(`python3 ../hardware/main.py ${command}`, (err, stdout, stderr) => {
+      if (err) {
         console.error("Error:", err);
         reject(err);
-            }
-            else {
-                resolve(stdout);
-            } });
+      } else {
+        resolve(stdout.trim());
+      }
     });
+  });
 }
 
 module.exports = { runPython };
