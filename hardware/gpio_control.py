d83105e1b3272304da70174b0d937e6fa2a61d23
l3nna <hkojic50@gmail.com>
2026-04-11 17:15:37 +0200

    refactor gpio_control.py to handle Raspberry Pi GPIO setup conditionally

--- hardware/gpio_control.py
+++ hardware/gpio_control.py
@@ -1,14 +1,24 @@
-import RPi.GPIO as GPIO
+try:
+    import RPi.GPIO as GPIO
+    REAL_PI = True
+except:
+    REAL_PI = False
 
-GPIO.setmode(GPIO.BCM)
-LED_PIN=17
-GPIO.setup(LED_PIN, GPIO.OUT)
+if REAL_PI:
+    GPIO.setmode(GPIO.BCM)
+    LED_PIN=17
+    GPIO.setup(LED_PIN, GPIO.OUT)
 
 def set_led(state):
-    if state == "true" :
-        GPIO.output(LED_PIN, GPIO.HIGH)
-        print("LED ON")
-
+    if REAL_PI:
+        if state == "true" :
+            GPIO.output(LED_PIN, GPIO.HIGH)
+            print("LED ON (REAL)")
+        else:
+            GPIO.output(LED_PIN, GPIO.LOW)
+            print("LED OFF (REAL)")
     else:
-        GPIO.output(LED_PIN, GPIO.LOW)
-        print("LED OFF")
+        if state == "true" :
+            print("LED ON (SIMULATED)")
+        else:
+            print("LED OFF (SIMULATED)")
